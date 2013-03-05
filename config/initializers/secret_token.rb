e33e518294dfda458438d31fe324e5e88621cb1f
KolomoetsOleg <KolomoetsOleg@yandex.ru>
2013-03-05 15:06:43 +0200

    Authentification is done

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebsiteOfTests::Application.config.secret_token = '251adb41c43df92e8b664f43ed8cadd47f602ae2e2e22621ec4fac34180ff2fa186a543bdc8b92f31259145575afd71db91740ae063a3f5c6be14edfd74468c8'
