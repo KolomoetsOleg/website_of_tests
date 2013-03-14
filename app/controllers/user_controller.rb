10b7db2aa920ee22c751009dfa3e3ba738a5e942
andreywerewolf <andrey.blazhey@gmail.com>
2013-03-14 18:52:19 +0200

    if user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,8 +2,12 @@ class UserController < ApplicationController
   
   def index
     @user = User.find(session["warden.user.user.key"][1].first)
+    @tests = Test.all
   end
   
+  def rezult
+    
+  end
 
   
 end
