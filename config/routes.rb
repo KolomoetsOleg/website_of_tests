60dcd3f871226f218c56349715eae4c20ead629a
andreywerewolf <andrey.blazhey@gmail.com>
2013-03-19 19:53:24 +0200

    routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ WebsiteOfTests::Application.routes.draw do
 
   get 'user/index'
 
+  post 'admin/update_roles'
 
   devise_for :users
 
@@ -10,10 +11,12 @@ WebsiteOfTests::Application.routes.draw do
   match '/userpage',   :to => 'user#userpage'
 
   match '/adminpage', :to => 'admin#adminpage'
+  match '/adminpage/show', :to => 'admin#show'
+  match '/admin/update_routes', :to => 'admin#show'
 
   resources :answers
   resources :tests
- # resources :quests
+  resources :quests
 
 
   resources :posts
