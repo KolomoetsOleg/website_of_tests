3e071c59552a38e0247248b8e622f918724cdb30
KolomoetsOleg <KolomoetsOleg@yandex.ru>
2013-03-20 16:19:37 +0200

    Pagination, russian messages, update list of tests

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,8 +2,9 @@ class Test < ActiveRecord::Base
   attr_accessible :active, :author_id, :kol_pop, :time, :title, :description
   has_many :quest
   has_many :rezult
+  validates :title,   :presence => true
   validates :kol_pop, :numericality => {:greater_than => 0, :less_than => 10}
-  validates :title,   :presence => {:format => '%{message}'}
+
   
   
 end
