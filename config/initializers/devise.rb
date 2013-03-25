cb73996e6cea0becef4a5acdaeb42a66758907b1
KolomoetsOleg <KolomoetsOleg@yandex.ru>
2013-03-25 15:04:21 +0200

    another email

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "KolomoetsOleg@yandex.ru"
+   config.mailer_sender = "kolomoets.oleg@googlemail.com"
 
 
   # Configure the class responsible to send e-mails.
