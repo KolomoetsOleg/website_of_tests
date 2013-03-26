b47f194aef0e65df771d7687a20dd1e9ab67be73
andreywerewolf <andrey.blazhey@gmail.com>
2013-03-26 12:41:34 +0200

    navigaion for test

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -25,38 +25,78 @@ class QuestsController < ApplicationController
 
 
 def testing
+  #
+  # session[:time]      - время до которого нужно закончить тест
+  # session[:quest_id]  - массив из id вопросов
+  # session[:answer_id] - хеш ответов юзера на вопросы {id(id вопроса), answer (ответ)}
+  # session[:status]    - масив статусов вопросов (на которые ответил, а на которы нет)
+  # params[:id]         - id вопроса из массива quest_id
+  # params[:id_quest]   - id вопроса на который было отвечено
+  # params[:answer]     - значение на которое ответел юзер
+  # session[:end]       - показатель конца масива
+  #
   @time = session[:time]
   @all_quest = session[:quest_id].count
   @page = params[:id].to_i
+  @quest = params[:id_quest].to_i
   @end = false
-  unless params[:id_quest].nil?
-  
-      if params[:answer].nil? 
-        session[:quest_id][@page - 1][params[:id_quest].to_i] = 0    
-      else
-        session[:quest_id][@page - 1][params[:id_quest].to_i] = 1
-      end    
-  end
-    session[:answer_id][params[:id_quest]] = params[:answer]
-    if params[:end].nil?
-        @quest = Quest.find(session[:quest_id][@page].keys.first)
-        @end = true if session[:quest_id][@page+=1].nil?
+
+  # Проверка было на вопрос отвечено или нажато кнопка "Пропустить"
+  unless params[:next].nil?
+    if params[:answer].nil? 
+      session[:status][session[:quest_id].index(@page)] = 0 
     else
-        redirect_to  :action => :finish
-    end
+      session[:status][session[:quest_id].index(@page)] = 1
+      # Внесение ответа в хеш ответов юзера
+      session[:answer_id][params[:id_quest]] = params[:answer]
+    end    
   end
 
-  def finish
-    @bal = Quest.check(session[:answer_id])
-    @bal = 0 if Time.now.getlocal("+02:00") > session[:time] + 60 # погрешность 1 минута на всякий случай)
-    @rezult = Rezult.where(:user_id => @user, :test_id => session[:test_id]).first
-    if @rezult.nil?
-      @rezult = Rezult.create(:test_id => session[:test_id], :user_id => @user, :bal => @bal)
+puts session[:status]
+puts session[:end]
+  if session[:end] == false
+    @quest = Quest.find(session[:quest_id][@page])
+    @page+=1
+    if session[:quest_id][@page].nil?
+      session[:end] = true
+    end
+  else
+   
+    if session[:status].index(0).nil?
+      redirect_to :action => :finish
     else
-     @rezult.update_attributes(:bal => @bal) if  @rezult.bal < @bal
-    end     
-    #@rezult.bal = @bal
+      @quest = Quest.find(session[:quest_id][session[:status].index(0)])  
+      @page = session[:quest_id].index(@quest.id)
+      @page +=1
+    end
+
   end
+
+
+
+ #if params[:end].nil? 
+ #@quest = Quest.find(session[:quest_id][@page].keys.first)
+ #  if session[:quest_id][@page+=1].nil? &&  
+ #   @end = true 
+ # else
+#@quest = Quest.find(session[:quest_id].first.keys.first)
+# end
+# else
+#   redirect_to  :action => :finish
+# end
+end
+
+def finish
+  @bal = Quest.check(session[:answer_id])
+  @bal = 0 if Time.now.getlocal("+02:00") > session[:time] + 60 # погрешность 1 минута на всякий случай)
+  @rezult = Rezult.where(:user_id => @user, :test_id => session[:test_id]).first
+  if @rezult.nil?
+    @rezult = Rezult.create(:test_id => session[:test_id], :user_id => @user, :bal => @bal)
+  else
+    @rezult.update_attributes(:bal => @bal) if  @rezult.bal < @bal
+  end     
+  #@rezult.bal = @bal
+end
 
   def new
     @quest = Quest.new
