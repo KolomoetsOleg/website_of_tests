a5095373b1072bd7788318c263fd5477d26479ab
Ilya <velheor.ilya@gmail.com>
2013-05-27 17:27:31 +0300

    deleted_unuseful_migration

--- db/migrate/20130527141447_rename_rez_on_heroku.rb
+++
@@ -1,9 +0,0 @@
-class RenameRezOnHeroku < ActiveRecord::Migration
-  def up
-  	rename_column :rezults, :test_id, :task_id
-  end
-
-  def down
-  	rename_column :rezults, :task_id, :test_id
-  end
-end
